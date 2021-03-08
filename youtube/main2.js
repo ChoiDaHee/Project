951a8da3bea1f99a29c7163d16479cc63c288309
ekgml0353 <vhxjekgml@naver.com>
2021-03-08 13:29:59 +0900

    youtube2

---
+++ youtube/main2.js
@@ -0,0 +1,6 @@
+const morebtn = document.querySelector('.Info .metadata .morebtn');
+const title = document.querySelector('.Info .metadata .title');
+
+morebtn.addEventListener('click', () = {
+    
+});
