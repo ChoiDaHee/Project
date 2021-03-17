88d3ab0a60b0a322f942a8410e123d1266e22450
ekgml0353 <vhxjekgml@naver.com>
2021-03-17 16:15:11 +0900

    js

--- javascript/variable.js
+++ javascript/variable.js
@@ -10,3 +10,20 @@
     console.log(name);
 }
 
+
+//var 
+//var hoisting
+age = 4;
+var age;
+
+
+// 3. constants (한번 할당하면 값이 젇대 바뀌지않음)
+
+const daysInWeek = 7;
+const maxNumber = 5;
+
+//4. Variable types
+
+
+
+
