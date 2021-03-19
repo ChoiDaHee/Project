d37f81f3a3757fb4fb32a3626bde16a7f2110063
ekgml0353 <vhxjekgml@naver.com>
2021-03-19 18:03:03 +0900

    js

--- javascript/variable.js
+++ javascript/variable.js
@@ -72,3 +72,8 @@ console.log(`value: ${x}, type: ${typeof x}`);
 //symbol
 const symbol1 = Symbol('id');
 const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+const gsymbol1 = Symbol.for('id');
+const gsymbol2 = Symbol.for('id');
+console.log(gsymbol1 === gsymbol2); //true
+console.log(`value: ${symbol1.description}, type: ${typeof Symbol1}`);
