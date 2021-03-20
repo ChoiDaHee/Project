33a0a892c7ed612e722855d5617e14f4c635dd46
ekgml0353 <vhxjekgml@naver.com>
2021-03-20 15:28:19 +0900

    operators

--- javascript/operator.js
+++ javascript/operator.js
@@ -2,3 +2,18 @@
 console.log('my'+'cat');
 console.log('1'+2);
 console.log(`string literals: 1 + 2 = ${1+2}`);
+
+// 2. Numeric operators
+console.log(1+1); //add
+console.log(1-1); //substract
+console.log(1/1); //divide
+console.log(1*1); //multiply
+console.log(5%2); //remainder
+console.log(2**3); //exponentiation
+
+// 3.Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+console.log(counter);
+console.log(preIncrement);
+
