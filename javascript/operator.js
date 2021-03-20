1a051d8dcb859ddfb90309947446fb389ef01e62
ekgml0353 <vhxjekgml@naver.com>
2021-03-20 17:28:13 +0900

    operators

--- javascript/operator.js
+++ javascript/operator.js
@@ -71,4 +71,29 @@ function check() {
 console.log(!value1);
 
 // 7. Equality
+const stringFive = '5';
+const numberFive = 5;
 
+// == loose equality, with type conversion
+console.log(stringFive == numberFive);
+console.log(stringFive != numberFive);
+
+// === strict equality, no type conversion
+console.log(stringFive === numberFive);
+console.log(stringFive !== numberFive);
+
+// object equality by reference
+const dahee1 = { name: 'dahee' };
+const dahee2 = { name: 'dahee' };
+const dahee3 = dahee1;
+console.log(dahee1 == dahee2);
+console.log(dahee1 === dahee2);
+console.log(dahee1 === dahee1);
+
+//equality - puzzler
+console.log(0 == false); //true
+console.log(0 === false); //false
+console.log('' == false); // true
+console.log('' === false); // false
+console.log(null == undefined); // true
+console.log(null === undefined); // false
