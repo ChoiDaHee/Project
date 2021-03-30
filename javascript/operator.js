a372f7e9d8e56cffc8dc59382fa00e0980cac945
ekgml0353 <vhxjekgml@naver.com>
2021-03-30 13:34:15 +0900

    js

--- javascript/operator.js
+++ javascript/operator.js
@@ -166,9 +166,11 @@ for (let i = 0; i < 10; i++) {
 }
 
 // break, continue
-// Q1. iterate from 0 to 10 and print only even numbers0 (use continue)
-
+// Q1. iterate from 0 to 10 and print only even numbers (use continue)
 
 // Q2. iterate from 0 to 10 and print numbers until reaching 8 (use break)
 
+for (i = 0; i < 9; i++) {
+    console.log(`for: ${i}`);
+}
 
